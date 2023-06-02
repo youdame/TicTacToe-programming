cfb453e7c77a89a13841cf611fb5fbeda387c282
youdame <112458620+youdame@users.noreply.github.com>
2023-06-02 10:25:44 +0900

    upload files...

--- ETTTP_TicTacToe_skeleton_updated.py
+++ ETTTP_TicTacToe_skeleton_updated.py
@@ -11,8 +11,7 @@
 
 import random
 import tkinter as tk
-# from socket import *
-import socket
+from socket import *
 import _thread
 
 SIZE=1024
@@ -355,38 +354,14 @@ class TTT(tk.Tk):
 
 # End of Root class
 
-    def check_msg(msg, recv_ip):
-        '''
-        Function that checks if received message is ETTTP format
-        '''
-        ###################  Fill Out  #######################
+def check_msg(msg, recv_ip):
+    '''
+    Function that checks if received message is ETTTP format
+    '''
+    ###################  Fill Out  #######################
 
-        
-
-
-        return True
-        ######################################################  
-
-if __name__ == '__main__':
-    SERVER_IP = '127.0.0.1'
-    MY_IP = '127.0.0.1'
-    SERVER_PORT = 12000
-    SIZE = 1024
-    SERVER_ADDR = (SERVER_IP, SERVER_PORT)
     
-    with socket(AF_INET, SOCK_STREAM) as client_socket:
-        client_socket.connect(SERVER_ADDR)
-        
-        # 서버로부터 먼저 시작할 플레이어를 받음
-        start = client_socket.recv(SIZE).decode()  # 서버로부터 시작 정보를 수신
-        start = int(start)
-        
-        # ACK 전송
-        ack = 'ACK'
-        client_socket.send(ack.encode())  # ACK를 서버에게 전송
-        
-        # 게임 시작
-        root = TTT(target_socket=client_socket, src_addr=MY_IP, dst_addr=SERVER_IP)
-        root.play(start_user=start)
-        root.mainloop()
-        client_socket.close()
+
+
+    return True
+    ######################################################  
