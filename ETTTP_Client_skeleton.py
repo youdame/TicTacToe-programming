30a43dd1f0d5d20bc90db8d2353a1987f7b7c988
youdame <112458620+youdame@users.noreply.github.com>
2023-06-02 10:09:16 +0900

    upload all skeleton codes

---
+++ ETTTP_Client_skeleton.py
@@ -0,0 +1,49 @@
+'''
+  ETTTP_Client_skeleton.py
+ 
+  34743-02 Information Communications
+  Term Project on Implementation of Ewah Tic-Tac-Toe Protocol
+ 
+  Skeleton Code Prepared by JeiHee Cho
+  May 24, 2023
+ 
+ '''
+
+import random
+import tkinter as tk
+from socket import *
+import _thread
+
+from ETTTP_TicTacToe_skeleton import TTT, check_msg
+    
+
+
+if __name__ == '__main__':
+
+    SERVER_IP = '127.0.0.1'
+    MY_IP = '127.0.0.1'
+    SERVER_PORT = 12000
+    SIZE = 1024
+    SERVER_ADDR = (SERVER_IP, SERVER_PORT)
+
+    
+    with socket(AF_INET, SOCK_STREAM) as client_socket:
+        client_socket.connect(SERVER_ADDR)  
+        
+        ###################################################################
+        # Receive who will start first from the server
+    
+    
+        ######################### Fill Out ################################
+        # Send ACK 
+        
+        
+        ###################################################################
+        
+        # Start game
+        root = TTT(target_socket=client_socket, src_addr=MY_IP,dst_addr=SERVER_IP)
+        root.play(start_user=start)
+        root.mainloop()
+        client_socket.close()
+        
+        
---
+++ ETTTP_Server_skeleton.py
@@ -0,0 +1,52 @@
+'''
+  ETTTP_Sever_skeleton.py
+ 
+  34743-02 Information Communications
+  Term Project on Implementation of Ewah Tic-Tac-Toe Protocol
+ 
+  Skeleton Code Prepared by JeiHee Cho
+  May 24, 2023
+ 
+ '''
+
+import random
+import tkinter as tk
+from socket import *
+import _thread
+
+from ETTTP_TicTacToe_skeleton import TTT, check_msg
+
+    
+if __name__ == '__main__':
+    
+    global send_header, recv_header
+    SERVER_PORT = 12000
+    SIZE = 1024
+    server_socket = socket(AF_INET,SOCK_STREAM)
+    server_socket.bind(('',SERVER_PORT))
+    server_socket.listen()
+    MY_IP = '127.0.0.1'
+    
+    while True:
+        client_socket, client_addr = server_socket.accept()
+        
+        start = random.randrange(0,2)   # select random to start
+        
+        ###################################################################
+        # Send start move information to peer
+    
+    
+        ######################### Fill Out ################################
+        # Receive ack - if ack is correct, start game
+        
+        
+        ###################################################################
+        
+        root = TTT(client=False,target_socket=client_socket, src_addr=MY_IP,dst_addr=client_addr[0])
+        root.play(start_user=start)
+        root.mainloop()
+        
+        client_socket.close()
+        
+        break
+    server_socket.close()
